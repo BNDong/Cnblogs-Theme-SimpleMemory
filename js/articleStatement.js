ff9522c05859171632efdc9056812d96f8cac30f
BNDong <dbnuo@foxmail.com>
2018-09-21 14:02:55 +0800

    全局配置迁移

--- js/articleStatement.js
+++ js/articleStatement.js
@@ -72,21 +72,20 @@ $(document).ready(function(){
     }
 
     bottom_html += '<br>';
-    if (window.cnblogsConfig.aboutHtml != "") {
-        console.log(window.cnblogsConfig);
-        bottom_html += window.cnblogsConfig.aboutHtml;
+    if (window.cnblogsConfig.essaySuffix.aboutHtml != "") {
+        bottom_html += '<span style="font-weight: bold; white-space:nowrap;">关于博主</span>：'+window.cnblogsConfig.essaySuffix.aboutHtml;
     } else {
         bottom_html += '<span style="font-weight: bold; white-space:nowrap;">关于博主</span>：编程路上的小学生，热爱技术，喜欢专研。评论和私信会在第一时间回复。或者<a href="http://msg.cnblogs.com/msg/send/'+tmp[3]+'" target="_blank">直接私信</a>我。';
     }
     bottom_html += '<br>';
-    if (window.cnblogsConfig.copyrightHtml != "") {
-        bottom_html += window.cnblogsConfig.copyrightHtml;
+    if (window.cnblogsConfig.essaySuffix.copyrightHtml != "") {
+        bottom_html += '<span style="font-weight: bold; white-space:nowrap;">版权声明</span>：'+window.cnblogsConfig.essaySuffix.copyrightHtml;
     } else {
         bottom_html += '<span style="font-weight: bold; white-space:nowrap;">版权声明</span>：署名 - 非商业性使用 - 禁止演绎，<a href="https://creativecommons.org/licenses/by-nc-nd/4.0/" alt="协议普通文本" title="协议普通文本" target="_blank">协议普通文本</a> | <a href="https://creativecommons.org/licenses/by-nc-nd/4.0/legalcode" alt="协议法律文本" title="协议法律文本" target="_blank">协议法律文本</a>。';
     }
     bottom_html += '<br>';
-    if (window.cnblogsConfig.supportHtml != "") {
-        bottom_html += window.cnblogsConfig.supportHtml;
+    if (window.cnblogsConfig.essaySuffix.supportHtml != "") {
+        bottom_html += '<span style="font-weight: bold; white-space:nowrap;">声援博主</span>：'+window.cnblogsConfig.essaySuffix.supportHtml;
     } else {
         bottom_html += '<span style="font-weight: bold; white-space:nowrap;">声援博主</span>：如果您觉得文章对您有帮助，可以点击文章右下角<strong><span style="color: #ff0000; font-size: 12pt;">【<a id="post-up" onclick="votePost('+uid+',\'Digg\')" href="javascript:void(0);">推荐</a>】</span></strong>一下。您的鼓励是博主的最大动力！';
     }
