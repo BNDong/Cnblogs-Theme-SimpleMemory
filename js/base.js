01195d40a798bbc9d36f760ed13fe4d50cc1a5d0
BNDong <dbnuo@foxmail.com>
2018-10-24 12:19:41 +0800

    Update base.js

--- js/base.js
+++ js/base.js
@@ -627,7 +627,7 @@ function Base() {
 
         // 添加我的标签
         if (toptags.length > 0 && menuToptags.html() == '')
-            menuToptags.html(getMenuData(toptags, 'icon-task_fill')).prev('.m-list-title').show();
+            menuToptags.html(getMenuData(toptags, 'icon-label_fill')).prev('.m-list-title').show();
 
         // 添加随笔分类
         if (sbClassify.length > 0 && menuClassify.html() == '')
@@ -635,7 +635,7 @@ function Base() {
 
         // 添加随笔档案
         if (sbRecord.length > 0 && menuRecord.html() == '')
-            menuRecord.html(getMenuData(sbRecord, 'icon-activity_fill')).prev('.m-list-title').show();
+            menuRecord.html(getMenuData(sbRecord, 'icon-task_fill')).prev('.m-list-title').show();
 
         // 添加阅读排行
         if (sbTopview.length > 0 && menuTopview.html() == '')
