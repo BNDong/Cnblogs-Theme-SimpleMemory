e1053a6cfdf3b0dfa11f6207b410d2c2d7c482fe
BNDong <dbnuo@foxmail.com>
2019-04-26 16:29:41 +0800

    更新字体库源

--- js/config.js
+++ js/config.js
@@ -87,7 +87,7 @@ require.config({
             deps: [
                 'tools',
                 'css!https://at.alicdn.com/t/font_543384_ezv3l7gd9r7.css', // 阿里云字体图标
-                'css!https://cdn.webfont.youziku.com/webfonts/nomal/111379/47284/5a531cbbf629dc07a8fbd011.css', // 有字库字体
+                'css!https://fonts.googleapis.com/css?family=Playball', // 谷歌字体库字体
             ]
         }
     }
