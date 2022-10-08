7f92cce3c87f2db5192211ee35d1c424cfeb4c30
laggage <38829279+laggage@users.noreply.github.com>
2022-10-08 19:02:27 +0800

    fix(#316/img-box): add other necessory attrs to img element (#317)

--- src/components/imgBox/imgBox.js
+++ src/components/imgBox/imgBox.js
@@ -29,10 +29,14 @@ export default function main(_) {
                 if (!tem.hasClass('code_img_closed') && !tem.hasClass('code_img_opened')) {
                     let width  = tem.attr('width');
                     let height = tem.attr('height');
+                    let alt = tem.attr('alt') ?? '';
+                    let style = tem.attr('style') ?? ''
                     tem.after('<a data-fancybox="gallery" href="'+tem.attr('src')+'"><img ' +
                         (width ? ' width="' + width + '" ' : '') +
                         (height ? ' height="' + height + '" ' : '') +
-                        ' src="'+tem.attr('src')+'"/></a>');
+                        ` src="${tem.attr('src')}"` +
+                        ` alt="${alt}"` +
+                        ` style="${style}"` + '/></a>');
                     tem.remove();
                 }
             });
