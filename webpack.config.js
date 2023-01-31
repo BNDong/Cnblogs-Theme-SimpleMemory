bf417a301e34092603302e01c23671544183d727
Alson <38209301+fzxiang@users.noreply.github.com>
2023-01-31 03:50:03 +0000

    fix: 修正fork仓库后打包出错，未配置字体文件loader

--- webpack.config.js
+++ webpack.config.js
@@ -90,6 +90,18 @@ module.exports = {
                     minimize: true,
                 },
             },
+            {
+                test: /\.(woff(2)?|ttf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                        name: '[name].[ext]',
+                        outputPath: 'fonts/'
+                        }
+                    }
+                ]
+            },
         ],
     },
 };
